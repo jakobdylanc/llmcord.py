19d8ae94a22ad4cc7e2b2c24975bee100278abcc
JakobDylanC <jakobdylanc@gmail.com>
2023-11-19 23:58:13 -0500

    add vision support

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ The best ChatGPT experience for your Discord server.
 
 # Features
 ### REPLY-BASED CHAT HISTORY
-Mention (@) the bot and it will reply to you. Reply to any message to continue the conversation from that point. 
-The chat history is built dynamically from reply chains.
+Mention (@) the bot and it will reply to you. Reply to any message to continue the conversation from that point. The chat history is built from reply chains.
 
 ### STREAMED RESPONSES
 The bot's replies start generating instantly rather than having to wait longer for one big chunk.
-> As of writing this no other GPT Discord bot has successfully implemented this. Feel free to prove me wrong :)
+
+### VISION SUPPORT
+The bot can see image attachments when set to the ***gpt-4-turbo-vision*** model.
 
 ### AND MORE...
 - Easily set a custom personality
@@ -28,12 +29,12 @@ pip install -r requirements.txt
 
 | Setting | Instructions |
 | --- | --- |
-| DISCORD\_BOT_TOKEN | Create a new Discord application at [discord.com/developers/applications](https://discord.com/developers/applications) and generate a token under the "Bot" tab. Also enable "message content intent". |
+| DISCORD\_BOT_TOKEN | Create a new Discord application at [discord.com/developers/applications](https://discord.com/developers/applications) and generate a token under the ***Bot*** tab. Also enable ***MESSAGE CONTENT INTENT***. |
 | OPENAI\_API_KEY | Generate an OpenAI API key at [platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys). You must also add a payment method to your OpenAI account at [platform.openai.com/account/billing/payment-methods](https://platform.openai.com/account/billing/payment-methods).|
-| GPT_MODEL | Choose either "gpt-3.5-turbo" or "gpt-4-turbo". More info on OpenAI models: [platform.openai.com/docs/models](https://platform.openai.com/docs/models) |
+| GPT_MODEL | Choose ***gpt-3.5-turbo***, ***gpt-4-turbo*** or ***gpt-4-turbo-vision***. More info on OpenAI models: [platform.openai.com/docs/models](https://platform.openai.com/docs/models) |
 | CUSTOM\_SYSTEM_PROMPT | Write practically anything you want to customize the bot's behavior! |
 
-3. Invite the bot to your Discord server with this URL (replace <CLIENT_ID> with your Discord application's client ID found under the "OAuth2" tab):
+3. Invite the bot to your Discord server with this URL (replace <CLIENT_ID> with your Discord application's client ID found under the ***OAuth2*** tab):
 ```plaintext
 https://discord.com/api/oauth2/authorize?client_id=<CLIENT_ID>&permissions=412317273088&scope=bot
 ```
--- gpt-discord.py
+++ gpt-discord.py
@@ -1,13 +1,13 @@
 import asyncio, discord, dotenv, logging, openai, os, tiktoken, time
 dotenv.load_dotenv()
-os.environ["GPT_MODEL"] = os.environ["GPT_MODEL"].replace("gpt-3.5-turbo", "gpt-3.5-turbo-1106", 1).replace("gpt-4-turbo", "gpt-4-1106-preview", 1)
+os.environ["GPT_MODEL"] = os.environ["GPT_MODEL"].replace("gpt-3.5-turbo", "gpt-3.5-turbo-1106", 1).replace("gpt-4-turbo-vision", "gpt-4-vision-preview", 1).replace("gpt-4-turbo", "gpt-4-1106-preview", 1)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s.%(msecs)03d %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 openai_client = openai.AsyncOpenAI()
 encoding = tiktoken.get_encoding("cl100k_base")
 SYSTEM_PROMPT = {"role": "system", "content": f"{os.environ['CUSTOM_SYSTEM_PROMPT']}\nUser's names are their Discord IDs and should be typed as '<@ID>'."}
-EXTRA_TOKENS_PER = {"message": 3, "name": 1, "reply": 3}
-MAX_TOTAL_TOKENS = {"gpt-3.5-turbo-1106": 16385, "gpt-4-1106-preview": 128000}
+EXTRA_TOKENS_PER = {"image_url": 85, "message": 3, "name": 1, "reply": 3}
+MAX_TOTAL_TOKENS = {"gpt-3.5-turbo-1106": 16385, "gpt-4-1106-preview": 128000, "gpt-4-vision-preview": 128000}
 MAX_COMPLETION_TOKENS = 1024
 
 intents = discord.Intents.default()
@@ -20,11 +20,12 @@ EMBED_MAX_LENGTH = 4096
 EDITS_PER_SECOND = 1.3
 
 
-def count_tokens(msg):
-    num_tokens = EXTRA_TOKENS_PER["message"]
+def count_tokens(msg, num_tokens=EXTRA_TOKENS_PER["message"]):
     for key, value in msg.items():
-        num_tokens += len(encoding.encode(value))
-        if key == "name": num_tokens += EXTRA_TOKENS_PER["name"]
+        if key == "content" and isinstance(value, list):
+            for item in value: num_tokens += count_tokens(item, num_tokens=0)
+        elif key not in ("image_url", "type"): num_tokens += len(encoding.encode(value))
+        if key in EXTRA_TOKENS_PER: num_tokens += EXTRA_TOKENS_PER[key]
     return num_tokens
 MAX_PROMPT_TOKENS_ADJUSTED = MAX_TOTAL_TOKENS[os.environ["GPT_MODEL"]] - MAX_COMPLETION_TOKENS - EXTRA_TOKENS_PER["reply"] - count_tokens(SYSTEM_PROMPT)
 
@@ -42,7 +43,7 @@ class MsgNode:
         while current_node != None:
             num_tokens += current_node.tokens
             if num_tokens > max_tokens: break
-            msgs.append(current_node.msg)
+            msgs += [current_node.msg]
             current_node = current_node.reference_node
         return msgs[::-1]
 
@@ -51,7 +52,7 @@ class MsgNode:
 async def on_message(message):
 
     # Filter out messages we don't want
-    if (message.channel.type != discord.ChannelType.private and discord_client.user not in message.mentions) or not message.content.replace(discord_client.user.mention, "", 1) or message.author.bot: return
+    if (message.channel.type != discord.ChannelType.private and discord_client.user not in message.mentions) or message.author.bot: return
 
     # If user replied to a message that's still generating, wait until it's done
     while message.reference and message.reference.message_id in in_progress_message_ids: await asyncio.sleep(0)
@@ -61,9 +62,10 @@ async def on_message(message):
         # Loop through message reply chain and create MsgNodes
         current_msg = message
         while True:
-            current_msg_content = current_msg.embeds[0].description if current_msg.author == discord_client.user else current_msg.content
-            if current_msg_content.startswith(discord_client.user.mention): current_msg_content = current_msg_content.replace(discord_client.user.mention, "", 1).lstrip()
-            if not current_msg_content: break
+            current_msg_text = current_msg.embeds[0].description if current_msg.author == discord_client.user else current_msg.content
+            if current_msg_text.startswith(discord_client.user.mention): current_msg_text = current_msg_text[len(discord_client.user.mention):].lstrip()
+            current_msg_content = [{"type": "text", "text": current_msg_text}] if current_msg_text else []
+            if os.environ["GPT_MODEL"] == "gpt-4-vision-preview": current_msg_content += [{"type": "image_url", "image_url": {"url": att.url, "detail": "low"}} for att in current_msg.attachments if "image" in att.content_type]
             current_msg_author_role = "assistant" if current_msg.author == discord_client.user else "user"
             msg_nodes[current_msg.id] = MsgNode({"role": current_msg_author_role, "content": current_msg_content, "name": str(current_msg.author.id)})
             if "previous_msg_id" in locals(): msg_nodes[previous_msg_id].reference_node = msg_nodes[current_msg.id]
@@ -72,15 +74,14 @@ async def on_message(message):
                 msg_nodes[current_msg.id].reference_node = msg_nodes[current_msg.reference.message_id]
                 break
             previous_msg_id = current_msg.id
-            try:
-                current_msg = current_msg.reference.resolved if isinstance(current_msg.reference.resolved, discord.Message) else await message.channel.fetch_message(current_msg.reference.message_id)
+            try: current_msg = current_msg.reference.resolved if isinstance(current_msg.reference.resolved, discord.Message) else await message.channel.fetch_message(current_msg.reference.message_id)
             except (discord.NotFound, discord.HTTPException): break
 
         # Build conversation history from reply chain
         msgs = [SYSTEM_PROMPT] + msg_nodes[message.id].get_reference_chain()
 
         # Generate and send bot reply
-        logging.info(f"Generating response for prompt:\n{msgs[-1]['content']}")
+        logging.info(f"Generating response for content:\n{msgs[-1]['content']}")
         response_messages, response_message_contents = [], []
         async for part in await openai_client.chat.completions.create(model=os.environ["GPT_MODEL"], messages=msgs, max_tokens=MAX_COMPLETION_TOKENS, stream=True):
             current_content = part.choices[0].delta.content or ""
@@ -88,10 +89,10 @@ async def on_message(message):
                 if response_messages == [] or len(response_message_contents[-1]+previous_content) > EMBED_MAX_LENGTH:
                     reply_message = message if response_messages == [] else response_messages[-1]
                     embed_color = EMBED_COLOR["complete"] if current_content == "" else EMBED_COLOR["incomplete"]
-                    response_messages.append(await reply_message.reply(embed=discord.Embed(description=previous_content, color=embed_color), silent=True))
+                    response_messages += [await reply_message.reply(embed=discord.Embed(description=previous_content, color=embed_color), silent=True)]
                     in_progress_message_ids.append(response_messages[-1].id)
                     last_message_task_time = time.time()
-                    response_message_contents.append("")
+                    response_message_contents += [""]
                 response_message_contents[-1] += previous_content
                 if response_message_contents[-1] != previous_content:
                     final_message_edit = True if len(response_message_contents[-1]+current_content) > EMBED_MAX_LENGTH or current_content == "" else False

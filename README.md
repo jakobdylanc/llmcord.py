76114cc67cd70c35caba7081c0f2fa61bf0213e8
JakobDylanC <jakobdylanc@gmail.com>
2023-12-21 15:34:57 -0500

    add MAX_IMAGES and MAX_MESSAGES config options
    and remove token counting

--- README.md
+++ README.md
@@ -13,12 +13,12 @@
 
 # Features
 - ### BEST CHAT SYSTEM
-  The chat history is built from reply chains. "@" the bot and it will reply to you. Anyone can reply to the bot's message to continue the conversation from that point.
-  > Works when replying to ANY message in your server. Just "@" the bot!
+  @ the bot and it will reply to you. Anyone can reply to the bot's message to continue the conversation from that point. Build conversations with reply chains!
+  > Works when replying to ANY message in your server, just @ the bot!
 
 - ### STREAMED RESPONSES
   The bot's responses are dynamically generated just like ChatGPT.
-  > With intuitive color coding: orange = generating, green = done.
+  > With intuitive color coding (orange = generating, green = done).
 
 - ### VISION SUPPORT
   The bot can see your image attachments when set to a vision model.
@@ -40,11 +40,13 @@ pip install -r requirements.txt
 
 | Setting | Instructions |
 | --- | --- |
-| DISCORD\_BOT_TOKEN | Create a new Discord application at [discord.com/developers/applications](https://discord.com/developers/applications) and generate a token under the ***Bot*** tab. Also enable ***MESSAGE CONTENT INTENT***. |
-| OPENAI\_API_KEY | **Only required if you select an OpenAI model.** Generate an OpenAI API key at [platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys). You must also add a payment method to your OpenAI account at [platform.openai.com/account/billing/payment-methods](https://platform.openai.com/account/billing/payment-methods).|
-| MISTRAL\_API_KEY | **Only required if you select a Mistral model.** Generate a Mistral API key at [console.mistral.ai/user/api-keys](https://console.mistral.ai/user/api-keys). You must also add a payment method to your Mistral account at [console.mistral.ai/billing](https://console.mistral.ai/billing).|
-| LLM | [OpenAI models](https://platform.openai.com/docs/models):<br />&nbsp;&nbsp;&nbsp;**gpt-3.5-turbo-1106** (latest GPT-3.5 Turbo)<br />&nbsp;&nbsp;&nbsp;**gpt-4-1106-preview** (GPT-4 Turbo)<br />&nbsp;&nbsp;&nbsp;**gpt-4-vision-preview** (GPT-4 Turbo with vision)<br />[Mistral models](https://docs.mistral.ai/platform/endpoints):<br />&nbsp;&nbsp;&nbsp;**mistral-tiny** (Mistral-7B)<br />&nbsp;&nbsp;&nbsp;**mistral-small** (Mixtral-8X7B)<br />&nbsp;&nbsp;&nbsp;**mistral-medium** (Mistral internal prototype) |
-| CUSTOM\_SYSTEM_PROMPT | Write practically anything you want to customize the bot's behavior! |
+| **DISCORD\_BOT_TOKEN** | Create a new Discord application at [discord.com/developers/applications](https://discord.com/developers/applications) and generate a token under the **Bot** tab. Also enable **MESSAGE CONTENT INTENT**. |
+| **OPENAI\_API_KEY** | **Only required if you select an OpenAI model.** Generate an OpenAI API key at [platform.openai.com/account/api-keys](https://platform.openai.com/account/api-keys). You must also add a payment method to your OpenAI account at [platform.openai.com/account/billing/payment-methods](https://platform.openai.com/account/billing/payment-methods).|
+| **MISTRAL\_API_KEY** | **Only required if you select a Mistral model.** Generate a Mistral API key at [console.mistral.ai/user/api-keys](https://console.mistral.ai/user/api-keys). You must also add a payment method to your Mistral account at [console.mistral.ai/billing](https://console.mistral.ai/billing).|
+| **LLM** | [OpenAI models](https://platform.openai.com/docs/models):<br />&nbsp;&nbsp;&nbsp;**gpt-3.5-turbo-1106** (latest GPT-3.5 Turbo)<br />&nbsp;&nbsp;&nbsp;**gpt-4-1106-preview** (GPT-4 Turbo)<br />&nbsp;&nbsp;&nbsp;**gpt-4-vision-preview** (GPT-4 Turbo with vision)<br />[Mistral models](https://docs.mistral.ai/platform/endpoints):<br />&nbsp;&nbsp;&nbsp;**mistral-tiny** (Mistral-7B)<br />&nbsp;&nbsp;&nbsp;**mistral-small** (Mixtral-8X7B)<br />&nbsp;&nbsp;&nbsp;**mistral-medium** (Mistral internal prototype) |
+| **MAX_IMAGES** | The maximum number of image attachments to accept in a single message. Only applicable when using a vision model. (Default: 5) |
+| **MAX_MESSAGES** | The maximum number of messages in a reply chain. (Default: 20) |
+| **CUSTOM\_SYSTEM_PROMPT** | Write practically anything you want to customize the bot's behavior! |
 
 3. Invite the bot to your Discord server with this URL (replace <CLIENT_ID> with your Discord application's client ID found under the ***OAuth2*** tab):
 ```plaintext
--- llmcord.py
+++ llmcord.py
@@ -1,19 +1,29 @@
 import asyncio
 import logging
 import os
-import time
-import dotenv
-import discord
-import openai
-import tiktoken
+from time import time
 
-dotenv.load_dotenv()
+import discord
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+
+load_dotenv()
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s.%(msecs)03d %(levelname)s: %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
+LLM_CONFIG = {
+    "gpt": {
+        "api_key": os.environ["OPENAI_API_KEY"],
+        "base_url": "https://api.openai.com/v1",
+    },
+    "mistral": {
+        "api_key": os.environ["MISTRAL_API_KEY"],
+        "base_url": "https://api.mistral.ai/v1",
+    },
+}
 SYSTEM_PROMPT = {
     "role": "system",
     "content": f"{os.environ['CUSTOM_SYSTEM_PROMPT']}\nUser's names are their Discord IDs and should be typed as '<@ID>'.",
@@ -24,74 +34,32 @@ if os.environ["LLM"] == "gpt-4-vision-preview" or "mistral-" in os.environ["LLM"
         "role": "system",
         "content": os.environ["CUSTOM_SYSTEM_PROMPT"],
     }
-if "gpt-" in os.environ["LLM"]:
-    LLM_API_KEY = os.environ["OPENAI_API_KEY"]
-    LLM_URL = "https://api.openai.com/v1"
-    if os.environ["LLM"] == "gpt-3.5-turbo-1106":
-        LLM_CONTEXT_WINDOW = 16385
-    elif os.environ["LLM"] in ("gpt-4-1106-preview", "gpt-4-vision-preview"):
-        LLM_CONTEXT_WINDOW = 128000
-elif "mistral-" in os.environ["LLM"]:
-    LLM_API_KEY = os.environ["MISTRAL_API_KEY"]
-    LLM_URL = "https://api.mistral.ai/v1"
-    LLM_CONTEXT_WINDOW = 32768
-EXTRA_TOKENS_PER = {"image_url": 85, "message": 3, "name": 1, "reply": 3}
-MAX_COMPLETION_TOKENS = 1024
-llm_client = openai.AsyncOpenAI(api_key=LLM_API_KEY, base_url=LLM_URL)
-encoding = tiktoken.get_encoding("cl100k_base")
 
+MAX_IMAGES = int(os.environ["MAX_IMAGES"])
+MAX_MESSAGES = int(os.environ["MAX_MESSAGES"])
+MAX_COMPLETION_TOKENS = 1024
 EMBED_COLOR = {"incomplete": discord.Color.orange(), "complete": discord.Color.green()}
 EMBED_MAX_LENGTH = 4096
 EDITS_PER_SECOND = 1.3
+
+llm_client = AsyncOpenAI(**LLM_CONFIG[os.environ["LLM"].split("-", 1)[0]])
 intents = discord.Intents.default()
 intents.message_content = True
 discord_client = discord.Client(intents=intents)
-msg_nodes = {}
+
+message_nodes = {}
 in_progress_message_ids = []
 
 
-def count_tokens(msg, num_tokens=EXTRA_TOKENS_PER["message"]):
-    for key, value in msg.items():
-        if key == "content" and isinstance(value, list):
-            for item in value:
-                num_tokens += count_tokens(item, num_tokens=0)
-        elif key not in ("image_url", "type"):
-            num_tokens += len(encoding.encode(value))
-        if key in EXTRA_TOKENS_PER:
-            num_tokens += EXTRA_TOKENS_PER[key]
-    return num_tokens
-
-
-MAX_PROMPT_TOKENS_ADJUSTED = (
-    LLM_CONTEXT_WINDOW
-    - MAX_COMPLETION_TOKENS
-    - EXTRA_TOKENS_PER["reply"]
-    - count_tokens(SYSTEM_PROMPT)
-)
-
-
-class MsgNode:
-    def __init__(self, msg, reference_node=None):
-        self.msg = msg
-        self.tokens = count_tokens(msg)
-        self.reference_node = reference_node
-
-    def get_reference_chain(self, max_tokens=MAX_PROMPT_TOKENS_ADJUSTED):
-        msgs = []
-        num_tokens = 0
-        current_node = self
-        while current_node is not None:
-            num_tokens += current_node.tokens
-            if num_tokens > max_tokens:
-                break
-            msgs += [current_node.msg]
-            current_node = current_node.reference_node
-        return msgs[::-1]
+class MessageNode:
+    def __init__(self, message, replied_to=None):
+        self.message = message
+        self.replied_to = replied_to
 
 
 @discord_client.event
 async def on_message(message):
-    # Filter out messages we don't want
+    # Filter out unwanted messages
     if (
         message.channel.type != discord.ChannelType.private
         and discord_client.user not in message.mentions
@@ -103,76 +71,85 @@ async def on_message(message):
         await asyncio.sleep(0)
 
     async with message.channel.typing():
-        # Loop through message reply chain and create MsgNodes
-        current_msg = message
-        previous_msg_id = None
+        # Loop through message reply chain and create MessageNodes
+        current_message = message
+        previous_message_id = None
         while True:
-            current_msg_text = (
-                current_msg.embeds[0].description
-                if current_msg.author == discord_client.user
-                else current_msg.content
+            current_message_text = (
+                current_message.embeds[0].description
+                if current_message.author == discord_client.user
+                else current_message.content
             )
-            if current_msg_text.startswith(discord_client.user.mention):
-                current_msg_text = current_msg_text[
+            if current_message_text.startswith(discord_client.user.mention):
+                current_message_text = current_message_text[
                     len(discord_client.user.mention) :
                 ].lstrip()
-            current_msg_content = (
-                [{"type": "text", "text": current_msg_text}] if current_msg_text else []
+            current_message_content = (
+                [{"type": "text", "text": current_message_text}]
+                if current_message_text
+                else []
             )
             if "vision" in os.environ["LLM"]:
-                current_msg_content += [
+                current_message_content += [
                     {
                         "type": "image_url",
                         "image_url": {"url": att.url, "detail": "low"},
                     }
-                    for att in current_msg.attachments
+                    for att in current_message.attachments
                     if "image" in att.content_type
-                ]
+                ][:MAX_IMAGES]
             else:  # Temporary fix until Mistral API supports message.content as a list
-                current_msg_content = current_msg_text
-            current_msg_author_role = (
-                "assistant" if current_msg.author == discord_client.user else "user"
+                current_message_content = current_message_text
+            current_message_author_role = (
+                "assistant" if current_message.author == discord_client.user else "user"
             )
-            msg_nodes[current_msg.id] = MsgNode(
+            message_nodes[current_message.id] = MessageNode(
                 {
-                    "role": current_msg_author_role,
-                    "content": current_msg_content,
-                    "name": str(current_msg.author.id),
+                    "role": current_message_author_role,
+                    "content": current_message_content,
+                    "name": str(current_message.author.id),
                 }
             )
-            if previous_msg_id:
-                msg_nodes[previous_msg_id].reference_node = msg_nodes[current_msg.id]
-            if not current_msg.reference:
+            if previous_message_id:
+                message_nodes[previous_message_id].replied_to = message_nodes[
+                    current_message.id
+                ]
+            if not current_message.reference:
                 break
-            if current_msg.reference.message_id in msg_nodes:
-                msg_nodes[current_msg.id].reference_node = msg_nodes[
-                    current_msg.reference.message_id
+            if current_message.reference.message_id in message_nodes:
+                message_nodes[current_message.id].replied_to = message_nodes[
+                    current_message.reference.message_id
                 ]
                 break
-            previous_msg_id = current_msg.id
+            previous_message_id = current_message.id
             try:
-                current_msg = (
-                    current_msg.reference.resolved
-                    if isinstance(current_msg.reference.resolved, discord.Message)
+                current_message = (
+                    current_message.reference.resolved
+                    if isinstance(current_message.reference.resolved, discord.Message)
                     else await message.channel.fetch_message(
-                        current_msg.reference.message_id
+                        current_message.reference.message_id
                     )
                 )
             except (discord.NotFound, discord.HTTPException):
                 break
 
         # Build conversation history from reply chain
-        msgs = [SYSTEM_PROMPT] + msg_nodes[message.id].get_reference_chain()
+        reply_chain = []
+        current_node = message_nodes[message.id]
+        while current_node is not None and len(reply_chain) < MAX_MESSAGES:
+            reply_chain += [current_node.message]
+            current_node = current_node.replied_to
+        messages = [SYSTEM_PROMPT] + reply_chain[::-1]
 
         # Generate and send bot reply
-        logging.info("Generating response for content: %s", msgs[-1])
+        logging.info("Generating response for content: %s", messages[-1])
         response_messages = []
         response_message_contents = []
         previous_content = None
         edit_message_task = None
         async for chunk in await llm_client.chat.completions.create(
             model=os.environ["LLM"],
-            messages=msgs,
+            messages=messages,
             max_tokens=MAX_COMPLETION_TOKENS,
             stream=True,
         ):
@@ -191,16 +168,17 @@ async def on_message(message):
                         if current_content == ""
                         else EMBED_COLOR["incomplete"]
                     )
+                    embed = discord.Embed(
+                        description=previous_content, color=embed_color
+                    )
                     response_messages += [
                         await reply_message.reply(
-                            embed=discord.Embed(
-                                description=previous_content, color=embed_color
-                            ),
+                            embed=embed,
                             silent=True,
                         )
                     ]
                     in_progress_message_ids.append(response_messages[-1].id)
-                    last_message_task_time = time.time()
+                    last_message_task_time = time()
                     response_message_contents += [""]
                 response_message_contents[-1] += previous_content
                 if response_message_contents[-1] != previous_content:
@@ -212,7 +190,7 @@ async def on_message(message):
                     if (
                         final_message_edit
                         or (not edit_message_task or edit_message_task.done())
-                        and time.time() - last_message_task_time
+                        and time() - last_message_task_time
                         >= len(in_progress_message_ids) / EDITS_PER_SECOND
                     ):
                         while edit_message_task and not edit_message_task.done():
@@ -222,26 +200,23 @@ async def on_message(message):
                             if final_message_edit
                             else EMBED_COLOR["incomplete"]
                         )
+                        embed.description = response_message_contents[-1]
+                        embed.color = embed_color
                         edit_message_task = asyncio.create_task(
-                            response_messages[-1].edit(
-                                embed=discord.Embed(
-                                    description=response_message_contents[-1],
-                                    color=embed_color,
-                                )
-                            )
+                            response_messages[-1].edit(embed=embed)
                         )
-                        last_message_task_time = time.time()
+                        last_message_task_time = time()
             previous_content = current_content
 
-        # Create MsgNode(s) for bot reply message(s) (can be multiple if bot reply was long)
+        # Create MessageNode(s) for bot reply message(s) (can be multiple if bot reply was long)
         for response_message in response_messages:
-            msg_nodes[response_message.id] = MsgNode(
+            message_nodes[response_message.id] = MessageNode(
                 {
                     "role": "assistant",
                     "content": "".join(response_message_contents),
                     "name": str(discord_client.user.id),
                 },
-                reference_node=msg_nodes[message.id],
+                replied_to=message_nodes[message.id],
             )
             in_progress_message_ids.remove(response_message.id)
 

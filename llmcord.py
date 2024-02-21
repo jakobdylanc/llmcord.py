17ec219596aa2a384b11daf6cf86d616b1ea49c9
JakobDylanC <jakobdylanc@gmail.com>
2024-02-21 00:02:44 -0500

    add limit to msg_nodes
    and other asyncio goodness

--- llmcord.py
+++ llmcord.py
@@ -30,6 +30,7 @@ MAX_MESSAGE_WARNING = f"⚠️ Only using last {MAX_MESSAGES} messages"
 EMBED_COLOR = {"incomplete": discord.Color.orange(), "complete": discord.Color.green()}
 EMBED_MAX_LENGTH = 4096
 EDITS_PER_SECOND = 1.3
+MAX_MESSAGE_NODES = 100
 
 system_prompt_extras = []
 if any(env["LLM"].startswith(x) for x in ("gpt", "openai/gpt")) and "gpt-4-vision-preview" not in env["LLM"]:
@@ -50,7 +51,8 @@ activity = discord.CustomActivity(name=env["CUSTOM_DISCORD_STATUS"][:128] or "gi
 discord_client = discord.Client(intents=intents, activity=activity)
 
 msg_nodes = {}
-active_msg_ids = []
+msg_locks = {}
+last_task_time = None
 
 
 class MsgNode:
@@ -71,6 +73,8 @@ def get_system_prompt():
 
 @discord_client.event
 async def on_message(msg):
+    global msg_nodes, msg_locks, last_task_time
+
     # Filter out unwanted messages
     if (
         msg.channel.type not in ALLOWED_CHANNEL_TYPES
@@ -86,56 +90,57 @@ async def on_message(msg):
     user_warnings = set()
     curr_msg = msg
     while len(reply_chain) < MAX_MESSAGES:
-        if curr_msg.id not in msg_nodes:
-            curr_msg_role = "assistant" if curr_msg.author == discord_client.user else "user"
-            curr_msg_content = (curr_msg.embeds[0].description if curr_msg.embeds and curr_msg.author.bot else curr_msg.content) or " "
-            if curr_msg_content.startswith(discord_client.user.mention):
-                curr_msg_content = curr_msg_content.replace(discord_client.user.mention, "", 1).lstrip() or " "
-            curr_msg_img_urls = [att.url for att in curr_msg.attachments if "image" in att.content_type]
-            if VISION_LLM:
-                curr_msg_content = [{"type": "text", "text": curr_msg_content}]
-                curr_msg_content += [{"type": "image_url", "image_url": {"url": url, "detail": "low"}} for url in curr_msg_img_urls[:MAX_IMAGES]]
-            msg_nodes[curr_msg.id] = MsgNode(
-                {
-                    "role": curr_msg_role,
-                    "content": curr_msg_content,
-                    "name": str(curr_msg.author.id),
-                },
-                too_many_images=len(curr_msg_img_urls) > MAX_IMAGES,
-            )
+        async with msg_locks.setdefault(curr_msg.id, asyncio.Lock()):
+            if curr_msg.id not in msg_nodes:
+                curr_msg_role = "assistant" if curr_msg.author == discord_client.user else "user"
+                curr_msg_content = (curr_msg.embeds[0].description if curr_msg.embeds and curr_msg.author.bot else curr_msg.content) or " "
+                if curr_msg_content.startswith(discord_client.user.mention):
+                    curr_msg_content = curr_msg_content.replace(discord_client.user.mention, "", 1).lstrip() or " "
+                curr_msg_img_urls = [att.url for att in curr_msg.attachments if "image" in att.content_type]
+                if VISION_LLM:
+                    curr_msg_content = [{"type": "text", "text": curr_msg_content}]
+                    curr_msg_content += [{"type": "image_url", "image_url": {"url": url, "detail": "low"}} for url in curr_msg_img_urls[:MAX_IMAGES]]
+                msg_nodes[curr_msg.id] = MsgNode(
+                    {
+                        "role": curr_msg_role,
+                        "content": curr_msg_content,
+                        "name": str(curr_msg.author.id),
+                    },
+                    too_many_images=len(curr_msg_img_urls) > MAX_IMAGES,
+                )
 
-            try:
-                if (
-                    not curr_msg.reference
-                    and discord_client.user.mention not in curr_msg.content
-                    and (prev_msg_in_channel := ([m async for m in curr_msg.channel.history(before=curr_msg, limit=1)] or [None])[0])
-                    and any(prev_msg_in_channel.type == type for type in (discord.MessageType.default, discord.MessageType.reply))
-                    and prev_msg_in_channel.author == curr_msg.author
-                ):
-                    msg_nodes[curr_msg.id].replied_to_msg = prev_msg_in_channel
-                else:
-                    next_is_thread_parent: bool = not curr_msg.reference and curr_msg.channel.type == discord.ChannelType.public_thread
-                    if next_msg_id := curr_msg.channel.id if next_is_thread_parent else getattr(curr_msg.reference, "message_id", None):
-                        while next_msg_id in active_msg_ids:
-                            await asyncio.sleep(0)
-                        msg_nodes[curr_msg.id].replied_to_msg = (
-                            (curr_msg.channel.starter_message or await curr_msg.channel.parent.fetch_message(next_msg_id))
-                            if next_is_thread_parent
-                            else (ref if isinstance(ref := curr_msg.reference.resolved, discord.Message) else await curr_msg.channel.fetch_message(next_msg_id))
-                        )
-            except (discord.NotFound, discord.HTTPException, AttributeError):
-                logging.exception("Error fetching next message in the chain")
+                try:
+                    if (
+                        not curr_msg.reference
+                        and discord_client.user.mention not in curr_msg.content
+                        and (prev_msg_in_channel := ([m async for m in curr_msg.channel.history(before=curr_msg, limit=1)] or [None])[0])
+                        and any(prev_msg_in_channel.type == type for type in (discord.MessageType.default, discord.MessageType.reply))
+                        and prev_msg_in_channel.author == curr_msg.author
+                    ):
+                        msg_nodes[curr_msg.id].replied_to_msg = prev_msg_in_channel
+                    else:
+                        next_is_thread_parent: bool = not curr_msg.reference and curr_msg.channel.type == discord.ChannelType.public_thread
+                        if next_msg_id := curr_msg.channel.id if next_is_thread_parent else getattr(curr_msg.reference, "message_id", None):
+                            while msg_locks.setdefault(next_msg_id, asyncio.Lock()).locked():
+                                await asyncio.sleep(0)
+                            msg_nodes[curr_msg.id].replied_to_msg = (
+                                (curr_msg.channel.starter_message or await curr_msg.channel.parent.fetch_message(next_msg_id))
+                                if next_is_thread_parent
+                                else (r if isinstance(r := curr_msg.reference.resolved, discord.Message) else await curr_msg.channel.fetch_message(next_msg_id))
+                            )
+                except (discord.NotFound, discord.HTTPException, AttributeError):
+                    logging.exception("Error fetching next message in the chain")
 
-        curr_node = msg_nodes[curr_msg.id]
-        reply_chain += [curr_node.data]
-        if curr_node.too_many_images:
-            user_warnings.add(MAX_IMAGE_WARNING)
-        if curr_node.replied_to_msg:
-            if len(reply_chain) == MAX_MESSAGES:
-                user_warnings.add(MAX_MESSAGE_WARNING)
-            curr_msg = curr_node.replied_to_msg
-        else:
-            break
+            curr_node = msg_nodes[curr_msg.id]
+            reply_chain += [curr_node.data]
+            if curr_node.too_many_images:
+                user_warnings.add(MAX_IMAGE_WARNING)
+            if curr_node.replied_to_msg:
+                if len(reply_chain) == MAX_MESSAGES:
+                    user_warnings.add(MAX_MESSAGE_WARNING)
+                curr_msg = curr_node.replied_to_msg
+            else:
+                break
 
     logging.info(f"Message received: {reply_chain[0]}, reply chain length: {len(reply_chain)}")
 
@@ -164,13 +169,13 @@ async def on_message(msg):
                             silent=True,
                         )
                     ]
-                    active_msg_ids.append(response_msgs[-1].id)
+                    await msg_locks.setdefault(response_msgs[-1].id, asyncio.Lock()).acquire()
                     last_task_time = dt.now().timestamp()
                     response_contents += [""]
 
                 response_contents[-1] += prev_content
                 is_final_edit: bool = chunk.choices[0].finish_reason or len(response_contents[-1] + curr_content) > EMBED_MAX_LENGTH
-                if is_final_edit or (not edit_task or edit_task.done()) and dt.now().timestamp() - last_task_time >= len(active_msg_ids) / EDITS_PER_SECOND:
+                if is_final_edit or (not edit_task or edit_task.done()) and dt.now().timestamp() - last_task_time >= EDITS_PER_SECOND:
                     while edit_task and not edit_task.done():
                         await asyncio.sleep(0)
                     if response_contents[-1].strip():
@@ -183,7 +188,7 @@ async def on_message(msg):
         except:
             logging.exception("Error while streaming response")
 
-    # Create MsgNode(s) for response message(s)
+    # Create MsgNodes for response messages
     for response_msg in response_msgs:
         msg_nodes[response_msg.id] = MsgNode(
             {
@@ -193,7 +198,14 @@ async def on_message(msg):
             },
             replied_to_msg=msg,
         )
-        active_msg_ids.remove(response_msg.id)
+        msg_locks[response_msg.id].release()
+
+    # Delete MsgNodes for oldest messages (lowest IDs)
+    if (num_nodes := len(msg_nodes)) > MAX_MESSAGE_NODES:
+        for msg_id in sorted(msg_nodes.keys())[: num_nodes - MAX_MESSAGE_NODES]:
+            async with msg_locks.setdefault(msg_id, asyncio.Lock()):
+                msg_nodes.pop(msg_id, None)
+                msg_locks.pop(msg_id, None)
 
 
 async def main():
